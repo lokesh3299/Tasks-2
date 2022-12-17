cac5e1cc7e7c14c7ac0a54de16240024c0d11d58
Lokesh <lokesh3299@gmail.com>
2022-12-17 20:17:16 +0530

    Update primenumbers.py

--- Prime Numbers/primenumbers.py
+++ Prime Numbers/primenumbers.py
@@ -1,15 +1,33 @@
-from termcolor import colored
+from colorama import Fore, Back, Style
 
 for num in range(1,201):
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
                 if num % 10 == 0:
-                    print(num)
+                    if(len(str(num))==1):
+                        print('  '+str(num))
+                    elif(len(str(num))==2):
+                        print(' '+str(num))
+                    elif(len(str(num))==3):
+                        print(str(num))
                 else:
-                    print(num , end="   ")
+                    if(len(str(num))==1):
+                        print('  '+str(num) , end="   ")
+                    elif(len(str(num))==2):
+                        print(' '+str(num) , end="   ")
+                    elif(len(str(num))==3):
+                        print(str(num) , end="   ")
                 break
         else:
-              print(colored(num , 'yellow'), end="   ")
+            if(len(str(num))==1):
+                print(Fore.YELLOW + Back.LIGHTBLACK_EX+'  '+str(num), end="   ")
+                print(Style.RESET_ALL, end='')
+            elif(len(str(num))==2):
+                print(Fore.YELLOW + Back.LIGHTBLACK_EX+' '+str(num), end="   ")
+                print(Style.RESET_ALL, end='')
+            elif(len(str(num))==3):
+                print(Fore.YELLOW + Back.LIGHTBLACK_EX+str(num), end="   ")
+                print(Style.RESET_ALL, end='')
     else:
-        print(num, end = '  ')
+        print('  '+str(num), end = "   ")
